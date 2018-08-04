93678fd56505427315ee0063942f706f4f4cf7b4
k32 <10274441+k32@users.noreply.github.com>
2018-08-04 17:33:41 +0200

    Tamed windows... more or less

--- erlstack-mode.el
+++ erlstack-mode.el
@@ -1,58 +1,79 @@
-;; [{shell,apply_fun,3,[{file,"shell.erl"},{line,907}]},
-;;  {erl_eval,do_apply,6,[{file,"erl_eval.erl"},{line,681}]},
-;;  {erl_eval,try_clauses,8,[{file,"erl_eval.erl"},{line,911}]},
-;;  {shell,exprs,7,[{file,"shell.erl"},{line,686}]},{shell,eval_exprs,7,[{file,"shell.erl"},{line,642}]},
-;;  {shell,eval_loop,3,[{file,"shell.erl"},{line,627}]}]
+;;; erlstack-mode.el --- Minor mode for analyzing Erlang stacktraces  -*- lexical-binding: t; -*-
 
+;; Author: k32
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(provide 'erlstack-mode)
 (require 'dash)
 
 (defun erlstack-whitespacify-concat (&rest re)
-  "Add whitespace match between to Erlang term regexp"
-  (--reduce (concat acc "[[:space:]]*" it) re))
+  "Intercalate strings with regexp matching whitespace"
+  (--reduce (concat acc "[ \t\n]*" it) re))
 
 (defvar erlstack-overlay nil)
 (defvar erlstack-code-overlay nil)
 (defvar erlstack-code-window nil)
 (defvar erlstack-code-window-active nil)
-(defvar erlstack-code-buffer "*Erlstack code*")
+(defvar erlstack-code-buffer nil)
 (defvar-local erlstack-buffer-file-name nil)
 (defvar-local erlstack-current-location nil)
 
 (defvar erlstack-frame-mode-map
   (make-sparse-keymap))
 
-(define-key erlstack-frame-mode-map (kbd "<return>") 'erlstack-visit-file)
-(define-key erlstack-frame-mode-map (kbd "C-<up>")   'erlstack-up-frame)
-(define-key erlstack-frame-mode-map (kbd "C-<down>") 'erlstack-down-frame)
+(define-key erlstack-frame-mode-map (kbd "C-<return>") 'erlstack-visit-file)
+(define-key erlstack-frame-mode-map (kbd "C-<up>")     'erlstack-up-frame)
+(define-key erlstack-frame-mode-map (kbd "C-<down>")   'erlstack-down-frame)
 
-(defvar erlstack-escaped-string-re
-  "\"\\(\\([^\"]\\|\\\"\\)*\\)\"")
+(defvar erlstack-string-re
+  "\"\\([^\"]*\\)\"")
 
 (defvar erlstack-file-re
-  (concat "{file," erlstack-escaped-string-re "}"))
+  (erlstack-whitespacify-concat "{" "file" "," erlstack-string-re "}"))
 
 (defvar erlstack-line-re
-  "{line,\\([[:digit:]]+\\)}")
+  (erlstack-whitespacify-concat "{" "line" "," "\\([[:digit:]]+\\)" "}"))
 
 (defvar erlstack-position-re
   (erlstack-whitespacify-concat "\\[" erlstack-file-re "," erlstack-line-re "]"))
 
 (defvar erlstack-stack-frame-re
-  (erlstack-whitespacify-concat "{.*" erlstack-position-re "}"))
+  (erlstack-whitespacify-concat "{[^{}]*" erlstack-position-re "}"))
 
 (defvar erlstack-stack-end-re
   "}]}")
 
 (defcustom erlstack-file-search-hook
-  '(erlstack-try-fullname)
+  '(erlstack-locate-abspath)
   "List of hooks used to search project files"
-  :options '(erlstack-try-fullname erlstack-remote-query)
+  :options '(erlstack-locate-abspath
+             erlstack-locate-otp
+             erlstack-locate-projectile)
   :group 'erlstack
   :type 'hook)
 
 (defcustom erlstack-lookup-window
   300
-  "Size of the lookup window"
+  "Size of lookup window"
   :group 'erlstack
   :type 'integer)
 
@@ -68,7 +89,7 @@
 (defun erlstack-frame-found (begin end)
   "This fuction is called when point enters stack frame"
   (let ((query       (match-string 1))
-        (line-number (string-to-number (match-string 3))))
+        (line-number (string-to-number (match-string 2))))
     (erlstack-try-show-file query line-number)
     (setq erlstack-overlay (make-overlay begin end))
     (set-transient-map erlstack-frame-mode-map t)
@@ -86,70 +107,104 @@
 
 (defun erlstack-code-popup (filename line-number)
   "Opens a pop-up window with the code"
-  (with-current-buffer (get-buffer-create erlstack-code-buffer)
-    (when (not (string= filename erlstack-buffer-file-name))
-      (message "Erlstack visits file: %s" filename)
-      (erase-buffer)
-      (insert-file-contents filename)
-      (setq-local erlstack-buffer-file-name filename)
-      (erlang-mode))
+  (setq erlstack-code-buffer (find-file-noselect filename t))
+  (with-current-buffer erlstack-code-buffer
     (with-no-warnings
       (goto-line line-number))
     (setq erlstack-code-buffer-posn (point))
-    (when erlstack-code-overlay
-      (delete-overlay erlstack-code-overlay))
     (setq erlstack-code-overlay (make-overlay
                                  (line-beginning-position)
                                  (line-end-position)))
-    (overlay-put erlstack-code-overlay 'face 'erlstack-frame-face))
-  (setq erlstack-code-window (display-buffer erlstack-code-buffer))
-  (setq erlstack-code-window-active t)
-  (set-window-point erlstack-code-window erlstack-code-buffer-posn))
+    (overlay-put erlstack-code-overlay 'face 'erlstack-frame-face)
+    (setq erlstack-code-window (display-buffer-in-side-window
+                                erlstack-code-buffer
+                                '((display-buffer-reuse-window
+                                   display-buffer-pop-up-window))))
+    (setq erlstack-code-window-active t)
+    (set-window-point erlstack-code-window erlstack-code-buffer-posn)))
 
 (defun erlstack-visit-file ()
   "Open file related to the currently selected stack frame for
 editing"
   (interactive)
-  (pcase erlstack-current-location
-    (`(,filename ,line-number)
-     (print erlstack-code-window)
-     (select-window erlstack-code-window)
-     (find-file filename)
-     (print `(,filename ,line-number))
-     (with-no-warnings
-       (goto-line line-number)))))
+  (when erlstack-code-window-active
+    (setq erlstack-code-window-active nil)
+    (pcase erlstack-current-location
+      (`(,filename ,line-number)
+       (select-window erlstack-code-window)
+       (with-no-warnings
+         (goto-line line-number))))))
 
-(defun erlstack-try-fullname (query line)
-  "Try search for local file matching full path"
+(defun erlstack-locate-abspath (query line)
+  "Try search for local file with absolute path"
   (when (file-exists-p query)
     query))
 
 (defun erlstack-frame-lost ()
   "This fuction is called when point leaves stack frame"
   (when erlstack-code-window-active
-    (message "Lost my window")
-    (switch-to-prev-buffer erlstack-code-window)
+    ;; (switch-to-prev-buffer erlstack-code-window)
+    (delete-side-window erlstack-code-window)
     (setq erlstack-code-window-active nil)))
 
 (defun erlstack-run-at-point ()
   "Attempt to analyze stack frame at the point"
   (interactive)
-  (when erlstack-overlay
-    (delete-overlay erlstack-overlay))
-  (while-no-input
-    (pcase (erlstack-parse-at-point)
-      (`(,begin ,end) (erlstack-frame-found begin end))
-      (_              (erlstack-frame-lost)))))
+  (run-with-idle-timer
+   0.1 nil
+   (lambda ()
+     (when erlstack-overlay
+       (delete-overlay erlstack-overlay))
+     (when erlstack-code-overlay
+       (delete-overlay erlstack-code-overlay))
+     (pcase (erlstack-parse-at-point)
+       (`(,begin ,end) (erlstack-frame-found begin end))
+       (_              (erlstack-frame-lost))))))
 
 (defun erlstack-parse-at-point ()
   "Attempt to find stacktrace at point"
   (save-excursion
-    (let ((begin (re-search-forward erlstack-stack-end-re
-                                    (+ (point) erlstack-lookup-window) t))
-          (end   (re-search-backward erlstack-stack-frame-re
+    (let ((point (point))
+          (end (re-search-forward erlstack-stack-end-re
+                                  (+ (point) erlstack-lookup-window) t))
+          (begin (re-search-backward erlstack-stack-frame-re
                                      (- (point) erlstack-lookup-window) t)))
-      (when (and begin end)
+      (when (and begin end (>= point begin))
         `(,begin ,end)))))
+
+(defun erlstack-goto-stack-begin ()
+  (goto-char (nth 0 (erlstack-parse-at-point))))
+
+(defun erlstack-goto-stack-end ()
+  (goto-char (nth 1 (erlstack-parse-at-point))))
+
+;; (defun erlstack-check-rebar-tmp-dir ()
+;;   "Check if the opened Erlang file is a temporary one created by
+;; rebar and prompt to open the original. Basically it detects
+;; presense of \"_build\" directory in the path and tries to guess
+;; original path"
+;;   (interactive)
+
+(defmacro erlstack-jump-frame (fun dir)
+  `(let* ((bound      (,dir (point) erlstack-lookup-window))
+          (next-frame (save-excursion
+                        (erlstack-goto-stack-begin)
+                        ,fun)))
+     (when next-frame
+       (goto-char next-frame)
+       (erlstack-goto-stack-begin))))
+
+(defun erlstack-up-frame ()
+  "Move one stack frame up"
+  (interactive)
+  (erlstack-jump-frame
+   (re-search-backward erlstack-file-re bound t) -))
+
+(defun erlstack-down-frame ()
+  "Move one stack frame down"
+  (interactive)
+  (erlstack-jump-frame
+   (re-search-forward erlstack-file-re bound t 2) +))
 
 (define-minor-mode erlstack-mode
  "Parse Erlang stacktrace under point and quickly navigate to the
@@ -161,3 +216,14 @@ line of the code"
  (if erlstack-mode
      (add-hook 'post-command-hook #'erlstack-run-at-point)
    (remove-hook 'post-command-hook #'erlstack-run-at-point)))
+
+;;; Example stacktrace:
+;;
+;; [{shell,apply_fun,3,[{file,"shell.erl"},{line,907}]},
+;;  {erl_eval,do_apply,6,[{file,"erl_eval.erl"},{line,681}]},
+;;  {erl_eval,try_clauses,8,[{file,"erl_eval.erl"},{line,911}]},
+;;  { shell , exprs , 7 , [{file,"shell.erl"},{line,686}]},{shell,eval_exprs,7,[{file,"shell.erl"},{line,642}]},
+;;  {shell,eval_loop,3,[ {file,"shell.erl"}, {line,627}]}]
+
+
+;;; erlstack-mode.el ends here
